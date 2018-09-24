b5dc8ef784691f74d4bb9081146d92d074265fbe
Artem <molchanov.artem.1994@gmail.com>
2018-09-24 13:53:03 +0600

    24.09.2018
    - catalog filter selector

---
+++ api/graphql/common/types/mutation/UserType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace api\graphql\common\types\mutation;
+
+use api\graphql\TypeRegistry;
+use api\graphql\MutationFieldsTypeInterface;
+use api\services\common\UserService;
+
+/**
+ * Class UserType
+ *
+ * @package api\graphql\common\types\mutation
+ */
+class UserType implements MutationFieldsTypeInterface
+{
+    /**
+     * @param TypeRegistry $typeRegistry
+     * @return array
+     */
+    public static function getMutationFieldsType(TypeRegistry $typeRegistry): array
+    {
+        return [
+            'userLogin' => [
+                'type' => $typeRegistry->string(),
+                'args' => [
+                    'login' => $typeRegistry->nonNull($typeRegistry->string()),
+                    'password' => $typeRegistry->nonNull($typeRegistry->string())
+                ],
+                'resolve' => function ($root, $args) {
+                    return (new UserService())->login($args['login'], $args['password']);
+                }
+            ]
+        ];
+    }
+
+}
