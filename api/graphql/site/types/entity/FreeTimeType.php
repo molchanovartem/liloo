9fb5c85f1322d7190ff3e6c598a058caaa8f1c6f
artem <molchanov.artem.1994@gmail.com>
2018-10-02 01:23:35 +0600

    02.10.2018
    - api FreeTimeType

--- api/graphql/site/types/entity/FreeTimeType.php
+++ api/graphql/site/types/entity/FreeTimeType.php
@@ -6,7 +6,9 @@ use api\graphql\QueryTypeInterface;
 use api\graphql\TypeRegistry;
 use api\models\Appointment;
 use api\models\MasterSchedule;
+use api\models\Service;
 use api\models\UserSchedule;
+use common\helpers\FreeDateTime;
 use common\services\ExecutorService;
 
 /**
@@ -49,17 +51,7 @@ class FreeTimeType implements QueryTypeInterface
                     ],
                 ],
                 'resolve' => function ($root, $args) {
-                    $executorService = new ExecutorService();
-                    $serviceSumTime = $executorService->getServiceSumTime($args['service_id']);
-                    $currentTime = sprintf('%02d:%02d', floor($serviceSumTime / 60), $serviceSumTime % 60);
                     $isSalon = empty($args['user_id']);
-                    $time = $isSalon ?
-                        $executorService->getCurrentTimeSalonMaster($args['master_id'], $args['date']) :
-                        $executorService->getCurrentTime($args['user_id'], $args['date']);
-
-                    if (empty($args['service_id'])) {
-                        return $time;
-                    }
 
                     $appointments = $isSalon ?
                         Appointment::find()
@@ -85,8 +77,59 @@ class FreeTimeType implements QueryTypeInterface
                                     ->asArray()
                                     ->all();
 
-                    return $executorService->getFreePartTime($time, $appointments, $currentTime, $args['date'], $schedule);
+                    $time = (new FreeDateTime($schedule, $appointments))->getPeriods(15);
+                    $partTime = [];
+
+                    foreach ($time as $t) {
+                        $partTime[] = $t;
+                    }
+
+                    if (empty($args['service_id'])) {
+                        return $partTime;
+                    }
+
+                    $serviceTime = Service::find()->where(['in', 'id', $args['service_id']])->sum('duration');
+
                 },
+//                'resolve' => function ($root, $args) {
+//                    $executorService = new ExecutorService();
+//                    $serviceSumTime = $executorService->getServiceSumTime($args['service_id']);
+//                    $currentTime = sprintf('%02d:%02d', floor($serviceSumTime / 60), $serviceSumTime % 60);
+//                    $isSalon = empty($args['user_id']);
+//                    $time = $isSalon ?
+//                        $executorService->getCurrentTimeSalonMaster($args['master_id'], $args['date']) :
+//                        $executorService->getCurrentTime($args['user_id'], $args['date']);
+//
+//                    if (empty($args['service_id'])) {
+//                        return $time;
+//                    }
+//
+//                    $appointments = $isSalon ?
+//                        Appointment::find()
+//                                   ->select(['start_date', 'end_date'])
+//                                   ->where(['master_id' => $args['master_id']])
+//                                   ->all() :
+//                        Appointment::find()
+//                                   ->select(['start_date', 'end_date'])
+//                                   ->where(['user_id' => $args['user_id']])
+//                                   ->all();
+//
+//                    $schedule = $isSalon ?
+//                        MasterSchedule::find()
+//                                      ->where(['master_id' => $args['master_id']])
+//                                      ->andWhere(['date(end_date)' => $args['date']])
+//                                      ->orderBy('end_date')
+//                                      ->asArray()
+//                                      ->all() :
+//                        UserSchedule::find()
+//                                    ->where(['user_id' => $args['user_id']])
+//                                    ->andWhere(['date(end_date)' => $args['date']])
+//                                    ->orderBy('end_date')
+//                                    ->asArray()
+//                                    ->all();
+//
+//                    return $executorService->getFreePartTime($time, $appointments, $currentTime, $args['date'], $schedule);
+//                },
             ],
         ];
     }
